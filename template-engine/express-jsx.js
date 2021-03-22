7ed40997f2dcd6c6605366690d38daa626c290ed
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-22 13:03:05 -0600

    Implementando un template engine

--- template-engine/express-jsx.js
+++ template-engine/express-jsx.js
@@ -1,6 +1,22 @@
 const fs = require("fs");
 
+function getKeysFromOptions(options) {
+  const { settings, _locals, ...objectKeys } = options;
+  return Object.keys(objectKeys);
+}
+
 function getRenderedContent(content, options) {
+  const keys = getKeysFromOptions(options);
+  let contentString = content.toString();
+
+  for (let key of keys) {
+    contentString = contentString.replace(
+      new RegExp(`\{${key}\}`, "gi"),
+      options[key]
+    );
+  }
+
+  return contentString;
 }
 
 function expressJxs(filePath, options, callback) {
