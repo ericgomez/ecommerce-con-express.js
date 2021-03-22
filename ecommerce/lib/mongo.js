95aa8cab5a825f858224ac99d9bf0592bd240e54
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-22 16:49:18 -0600

    Implementando acciones CRUD en MongoDB

--- ecommerce/lib/mongo.js
+++ ecommerce/lib/mongo.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const { config } = require("../config");
 
 const USER = encodeURIComponent(config.dbUser);
@@ -9,8 +9,7 @@ const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}
 
 class MongoLib {
   constructor() {
-    console.log('MONGO_URI', MONGO_URI);
-    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     this.dbName = DB_NAME;
   }
 
@@ -34,6 +33,38 @@ class MongoLib {
         .find(query)
         .toArray();
     });
+  }
+
+  get(collection, id) {
+    return this.connect().then(db => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
+  }
+
+  create(collection, data) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).insertOne(data);
+      })
+      .then(result => result.insertedId);
+  }
+
+  update(collection, id, data) {
+    return this.connect()
+      .then(db => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+      })
+      .then(result => result.upsertedId || id);
+  }
+
+  delete(collection, id) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+      })
+      .then(() => id);
   }
 }
 
