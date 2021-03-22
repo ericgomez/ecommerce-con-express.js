4a4573a5f41a652969b75560344670af93994e91
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-22 15:34:56 -0600

    Implementando nuestra capa de Servicios

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -13,6 +13,9 @@ app.set("view engine", "pug");
 app.use("/products", productsRouter);
 app.use("/api/products", productsApiRouter);
 
+// utilizamos express.json para procesar cuerpos que vienen en formato JSON
+app.use(express.json());
+
 const server = app.listen(8000, function() {
   console.log(`Listening http://localhost:${server.address().port}`);
 });
--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const ProductsService = require("../services/products");
 
-router.get("/", function(req, res) {
-  res.render("products", { products });
+const productService = new ProductsService();
+
+router.get("/", async function(req, res, next) {
+  const { tags } = req.query;
+  try {
+    const products = await productService.getProducts({ tags });
+    res.render("products", { products });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router; 
