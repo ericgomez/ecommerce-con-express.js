d7d77167240943fb5f9b8079e2ff28a75eef518e
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-23 12:26:53 -0600

    Qué es Joi y cómo configurar Joi

--- ecommerce/routes/api/products.js
+++ ecommerce/routes/api/products.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ProductsService = require("../../services/products");
+const validation = require("../../utils/middlewares/validationHandler");
+
+const {
+  productIdSchema,
+  productTagSchema,
+  createProductSchema,
+  updateProductSchema
+} = require("../../utils/schemas/products");
 
 const productService = new ProductsService();
 
@@ -10,7 +18,6 @@ router.get('/', async function(req, res, next) {
     console.log("req", req.query);
 
     try {
-      throw new Error('This is an error from the API');
       const products = await productService.getProducts({ tags });
 
       res.status(200).json({
@@ -39,12 +46,10 @@ router.get('/:productId', async function(req, res, next) {
     }
 });
 
-router.post('/', async function(req, res, next) {
+router.post('/', validation(createProductSchema), async function(req, res, next) {
     // ponemos un alizas para renombrar la palabra body por product
     const { body: product } = req;
     
-    console.log("req", req.body);
-
     try {
       const createdProduct = await productService.createProduct({ product });
 
@@ -57,7 +62,7 @@ router.post('/', async function(req, res, next) {
     }
 });
 
-router.put('/:productId', async function(req, res, next) {
+router.put('/:productId', validation({ productId: productIdSchema }, "params"), validation(updateProductSchema), async function(req, res, next) {
     const { productId } = req.params;
     const { body: product } = req;
   
